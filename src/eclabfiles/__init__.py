1c4ed844fed33c2dcc20eb42531494142ad354f4
vetschn <nicolas.vetsch@gmx.ch>
2021-10-18 09:28:08 +0200

    Updated gitignore and init

--- src/eclabfiles/__init__.py
+++ src/eclabfiles/__init__.py
@@ -1,2 +1,1 @@
-from .main import (mpr_to_df, mpr_to_csv, mpr_to_xlsx, mpt_to_df, mpt_to_csv,
-                   mpt_to_xlsx, mps_to_dfs, mps_to_csv, mps_to_xlsx)
+from .main import to_df, to_csv, to_xlsx
