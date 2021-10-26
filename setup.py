a8d9267920aed6d983a6c069cb71e237a3c85f0f
vetschn <nicolas.vetsch@gmx.ch>
2021-10-26 15:25:21 +0200

    Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="eclabfiles",
-    version="0.3.0",
+    version="0.3.1",
     author="Nicolas Vetsch",
     author_email="vetschnicolas@gmail.com",
     description="Parsing and converting of files from BioLogic's EC-Lab.",
