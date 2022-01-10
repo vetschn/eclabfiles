0610c99e2cb84a7a6a0aa9e466d65912bdc6b115
vetschn <nicolas.vetsch@gmx.ch>
2022-01-10 11:04:53 +0100

    Redid the main functionality

--- src/eclabfiles/main.py
+++ src/eclabfiles/main.py
@@ -3,39 +3,40 @@
 """Functions for converting parsed EC-Lab file data to DataFrame, .csv
 and .xlsx.
 
+See the individual parsers to get an idea of the data formats used.
+
 .. codeauthor:: Nicolas Vetsch <vetschnicolas@gmail.com>
 """
 import os
-from typing import Union
 
 import pandas as pd
 
 from eclabfiles import mpr, mps, mpt
 
 
-def _construct_path(other_path: str, ext: str) -> str:
-    """Constructs a new file path from the given path and extension.
+def _construct_fn(other_fn: str, ext: str) -> str:
+    """Constructs a new file name from the given name and extension.
 
     Parameters
     ----------
-    other_path
+    other_fn
         The path to some file.
     ext
-        The new extension to add to the other_path.
+        The new extension to add to the other_fn.
 
     Returns
     -------
     str
-        A new filepath with the given extension.
+        A new file name with the given extension.
 
     """
-    head, tail = os.path.split(other_path)
+    head, tail = os.path.split(other_fn)
     tail, __ = os.path.splitext(tail)
-    this_path = os.path.join(head, tail + ext)
-    return this_path
+    this_fn = os.path.join(head, tail + ext)
+    return this_fn
 
 
-def process(fn: str) -> Union[list, dict]:
+def process(fn: str, encoding: str = "windows-1252", **kwargs) -> tuple[dict, dict]:
     """Processes an EC-Lab file.
 
     The function finds the file extension and tries to choose the
@@ -44,36 +45,61 @@ def process(fn: str) -> Union[list, dict]:
     Parameters
     ----------
     fn
-        The path to an EC-Lab file (.mpt/.mpr/.mps)
+        The path to an EC-Lab file.
+    encoding
+        Encoding of ``fn``, by default "windows-1252".
 
     Returns
     -------
-    Union[dict, dict]
-        The processed file. Data files return 
+    tuple[dict, dict]
+        The processed file. Data files produce one dictionary containing
+        the data and another with metadata. Settings files produce a
+        dictionary containing the linked techniques, optionally with the
+        data from all techniques loaded in, and another dict with
+        metadata. See the respective parser for more information.
+
+    Other Parameters
+    ----------------
+    load_data
+        Whether to try and load data from the same folder as the given
+        settings file.
+    load_type
+        The type of file to load in. Defaults to using binary data
+        files. Possible options are "mpr" and "mpt".
 
     """
     __, ext = os.path.splitext(fn)
     if ext == ".mpt":
-        return mpt.process(fn)
+        return mpt.process(fn, encoding=encoding)
     if ext == ".mpr":
-        return mpr.process(fn)
+        return mpr.process(fn, encoding=encoding)
     if ext == ".mps":
-        return mps.process(fn)
+        return mps.process(
+            fn,
+            encoding=encoding,
+            load_data=kwargs.get("load_data", False),
+            load_type=kwargs.get("load_type", None),
+        )
     raise NotImplementedError(f"Unrecognized file extension: {ext}")
 
 
-def to_df(path: str) -> Union[pd.DataFrame, list[pd.DataFrame]]:
-    """Extracts the data from an EC-Lab file and returns it as Pandas
-    DataFrame(s)
+def to_df(fn: str, encoding: str = "windows-1252", **kwargs) -> pd.DataFrame:
+    """Extracts data from an EC-Lab file and returns it as DataFrame.
 
     The function finds the file extension and tries to choose the
-    correct parser. If the file is an .mps settings file, this returns a
-    MultiIndex Dataframe.
+    correct parser. The DataFrame.attrs will contain any metadata.
+
+    If the file is a settings file, this returns a MultiIndex Dataframe,
+    the first index being the technique number. The DataFrame.attrs will
+    contain the settings file metadata as well as all the linked
+    techniques' metadata.
 
     Parameters
     ----------
-    path
-        The path to an EC-Lab file (.mpt/.mpr/.mps)
+    fn
+        The path to an EC-Lab file.
+    encoding
+        Encoding of ``fn``, by default "windows-1252".
 
     Returns
     -------
@@ -82,94 +108,69 @@ def to_df(path: str) -> Union[pd.DataFrame, list[pd.DataFrame]]:
         techniques in an .mps file. Optionally also the parsed data is
         returned.
 
+    Other Parameters
+    ----------------
+    load_type
+        The type of file to load in. Defaults to using binary data
+        files. Possible options are "mpr" and "mpt".
+
     """
-    __, ext = os.path.splitext(path)
-    if ext == ".mpt" or ".mpr":
-        data, meta = process(path)
-        df = pd.DataFrame.from_dict(data)
+    __, ext = os.path.splitext(fn)
+    if ext in {".mpt", ".mpr"}:
+        data, meta = process(fn, encoding=encoding)
+        df = pd.DataFrame.from_records(data)
         df.attrs = meta
     elif ext == ".mps":
-        techniques, meta = mps.process(path)
-        # TODO
-        data = mps["data"]
-        dfs = []
-        if "mpt" in data.keys():
-            # It's intentional to prefer .mpt over .mpr files here as
-            # they often contain a few more columns than the .mpr files.
-            for mpt in data["mpt"]:
-                mpt_records = mpt["datapoints"]
-                mpt_df = pd.DataFrame.from_dict(mpt_records)
-                dfs.append(mpt_df)
-        elif "mpr" in data.keys():
-            for mpr in data["mpr"]:
-                mpr_records = mpr[1]["data"]["datapoints"]
-                mpr_df = pd.DataFrame.from_dict(mpr_records)
-                dfs.append(mpr_df)
-        else:
-            raise ValueError("The given .mps file does not contain any data.")
-        return dfs
+        techniques, meta = mps.process(
+            fn,
+            encoding=encoding,
+            load_data=True,
+            load_type=kwargs.get("load_type", "mpr"),
+        )
+        dfs = {}
+        for num, technique in techniques.items():
+            if "data" in technique:
+                dfs[num] = pd.DataFrame.from_records(technique.pop("data"))
+        df = pd.concat(dfs)
+        df.attrs = meta | techniques
     else:
         raise ValueError(f"Unrecognized file extension: {ext}")
     return df
 
 
-def to_csv(path: str, csv_path: str = None) -> None:
-    """Extracts the data from an .mpt/.mpr file or from the techniques
-    in an .mps file and writes it to a number of .csv files.
+def to_csv(fn: str, encoding: str = "windows-1252", csv_fn: str = None) -> None:
+    """Extracts the data from an EC-Lab file and writes it to csv.
 
     Parameters
     ----------
-    path
+    fn
         The path to the EC-Lab file to read in.
-    csv_path
-        Base path to use for the .csv files. The function automatically
-        appends the technique number to the file name. Defaults to
-        construct the .csv filename from the mpt_path.
+    encoding
+        Encoding of ``fn``, by default "windows-1252".
+    csv_fn
+        Base path to use for the csv file. Defaults to generate the csv
+        file name from the input file name.
 
     """
-    df = to_df(path)
-    if isinstance(df, pd.DataFrame):
-        if csv_path is None:
-            csv_path = _construct_path(path, ".csv")
-        df.to_csv(csv_path, float_format="%.15f", index=False)
-    elif isinstance(df, list):
-        for i, df in enumerate(df):
-            if csv_path:
-                df.to_csv(
-                    _construct_path(csv_path, f"_{i+1:02d}.csv"),
-                    float_format="%.15f",
-                    index=False,
-                )
-            df.to_csv(
-                _construct_path(path, f"_{i+1:02d}.csv"),
-                float_format="%.15f",
-                index=False,
-            )
+    df = to_df(fn, encoding=encoding)
+    if csv_fn is None:
+        csv_fn = _construct_fn(fn, ".csv")
+    df.to_csv(csv_fn, float_format="%.15f")
 
 
-def to_xlsx(path: str, xlsx_path: str = None) -> None:
-    """Extracts the data from an .mpt/.mpr file or from the techniques in
-    an .mps file and writes it to an Excel file.
-
-    If the file is an .mps, this method writes the data to numbered
-    worksheets in the Excel file.
+def to_excel(fn: str, encoding: str = "windows-1252", excel_fn: str = None) -> None:
+    """Extracts the data from an EC-Lab file and writes it to Excel.
 
     Parameters
     ----------
-    path
+    fn
         The path to the EC-Lab file to read in.
-    xlsx_path
-        Path to the Excel file to write. Defaults to construct the
-        filename from the mpt_path.
+    excel_fn
+        Path to the Excel file to write. Defaults to generate the xlsx
+        file name from the input file name.
 
     """
-    df = to_df(path)
-    if xlsx_path is None:
-        xlsx_path = _construct_path(path, ".xlsx")
-    if isinstance(df, pd.DataFrame):
-        df.to_excel(xlsx_path, index=False)
-    elif isinstance(df, list):
-        # pylint: disable=abstract-class-instantiated
-        with pd.ExcelWriter(xlsx_path) as writer:
-            for i, df in enumerate(df):
-                df.to_excel(writer, sheet_name=f"{i+1:02d}", index=False)
+    df = to_df(fn, encoding=encoding)
+    if excel_fn is None:
+        excel_fn = _construct_fn(fn, ".xlsx")
+    df.to_excel(excel_fn)
